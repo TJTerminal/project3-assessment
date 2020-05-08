2ab8db3a81a6258e7f5d1f19a5fdcc1b8fcdd27e
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-08 14:49:27 -0700

    makemigrations and migrate

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'items': items})
 
 def add(request):
     return render(request, 'add.html')
